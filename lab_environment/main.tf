24bd1961df06bdda4bae6d83a477e35d7262cab6
Aidan <aidanmadden22@gmail.com>
2022-02-12 11:39:38 -0600

    basic apache web server environment

--- lab_environment/main.tf
+++ lab_environment/main.tf
@@ -14,11 +14,128 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-033b95fb8079dc481"
-  instance_type = "t2.micro"
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/26"
 
   tags = {
-    Name = var.instance_name
+    Name = "main"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.32/27"
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/27"
+}
+
+resource "aws_route_table" "public_routes" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "public_routes"
+  }
+}
+
+resource "aws_route_table_association" "public_association" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_routes.id
+}
+
+resource "aws_security_group" "allow_inet" {
+  name        = "allow_inet"
+  description = "Allow internet inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "http access"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+    ingress {
+    description      = "https access"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "SSH from anywhere"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-033b95fb8079dc481"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.public.id
+  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id, aws_security_group.allow_inet.id]
+  user_data = file("userdata.sh")
+
+  tags = {
+    Name = var.web_server
+  }
+}
+
+resource "aws_instance" "db_server" {
+  ami           = "ami-033b95fb8079dc481"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.private.id
+
+  tags = {
+    Name = var.db_server
   }
 }
--- lab_environment/variables.tf
+++ lab_environment/variables.tf
@@ -1,5 +1,11 @@
-variable "instance_name" {
+variable "web_server" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string
-  default     = "ExampleAppServerInstance"
+  default     = "web01"
+}
+
+variable "db_server" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "db01"
 }
